e4960d1109135b267370ccfd02f7194488d938e5
William Stenberg <wilst118@ixtab.edu.isy.liu.se>
2018-04-09 16:31:23 +0200

    Added some operations

--- cpu_embryo/alu.vhd
+++ cpu_embryo/alu.vhd
@@ -9,15 +9,15 @@ entity alu is
     port(
       clk : in std_logic;
       alu_data : in unsigned(15 downto 0);
-      alu_opcode : in unsigned (3 downto 0));
-      ar : out unsigned (15 downto 0);
-      status : out unsigned 7 downto 0);
+      alu_opcode : in unsigned (3 downto 0);
+      ar : buffer unsigned (15 downto 0);
+      status : out unsigned (7 downto 0));
 end alu;
 
 architecture Behavioral of alu is
   signal equal : std_logic;
-  signal K_add : unsigned(15 downto 0);
-  signal K_sadd : unsigned(15 downto 0);  -- "special add" proof of concept
+	signal K_add : unsigned(15 downto 0);
+	signal K_sub : unsigned(15 downto 0);
   
 begin
   process(clk) begin
@@ -25,21 +25,40 @@ begin
 
       if alu_opcode = 1 then         -- add
         -- Overflow flag
-        status(0) <= '1' if (
-          ((ar(15) = alu_data(15)) and (alu_data(15) /= K_add(15)))
-          ) else '0';
-         ar <= K_add;        
+				if ((ar(15) = alu_data(15)) and (K_add(15) /= ar(15))) then
+					status(0) <= '1';
+				else
+					status(0) <= '0';
+				end if;
+        ar <= ar + alu_data;
       elsif alu_opcode = 2 then         -- sub
-        status(1) <= '1' if (equal = '1') else '0';
-        ar <= K_sub;
+				-- set overflow flag
+				if ((ar(15) = alu_data(15)) and (K_sub(15) /= ar(15))) then
+					status(0) <= '1';
+				else
+					status(0) <= '0';
+				end if;
+				-- zero flag (when subtracting equal terms)
+        status(1) <= equal;
+        ar <= ar - alu_data;
+			elsif alu_opcode = 3 then 				-- arithmetic/logic shift left
+				ar <= ar(14 downto 0) & '0';
+			elsif alu_opcode = 4 then 				-- arithmetic shift right
+				ar <= ar(15) & ar(15 downto 1);
+			elsif alu_opcode = 15 then 				-- set special flag
+				status(7) <= '1';	
       else                              -- idle
         ar <= ar;
       end if;
     end if;
   end process;
-
-  K_add <= ar + alu_data;
-  K_sub <= ar - alu_data;
-  equal <= '1' if (ar = alu_data) else '0';
+	
+	K_add <= ar + alu_data;
+	K_sub <= ar - alu_data;
+	equal <= '1' when (ar = alu_data) else '0';
+                                      
+                                      
+                                         
+                                       
   
 end Behavioral;
