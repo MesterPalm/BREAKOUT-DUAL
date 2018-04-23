64dcf00c4e8b47c29ffdb22ba40fc3abd44995e9
William Stenberg <wilst118@ixtab.edu.isy.liu.se>
2018-04-23 14:54:13 +0200

    Initial work on reflection

--- cpu_embryo/alu.vhd
+++ cpu_embryo/alu.vhd
@@ -18,9 +18,12 @@ architecture Behavioral of alu is
   signal equal : std_logic;
   signal K_add : unsigned(31 downto 0);
   signal K_sub : unsigned(31 downto 0);
+  signal normal : unsigned (2 downto 0); -- Collision normal vector
+	signal ball_v : unsigned (2 downto 0); -- Ball velocity vector
+	signal reflected : unsigned (2 downto 0); -- Reflected velocity vector 
   
 begin
-  process(clk) begin
+  process(clk, normal, ball_v) begin
     if (rising_edge(clk)) then
       if alu_opcode = 1 then       -- add
         -- Overflow flag
@@ -44,6 +47,21 @@ begin
         ar <= ar(30 downto 0) & '0';
       elsif alu_opcode = 4 then 				-- arithmetic shift right
         ar <= ar(31) & ar(31 downto 1);
+      elsif alu_opcode = 5 then         -- vector reflection
+				case normal is
+					when "0" =>
+						case ball_v is
+							when "0" =>
+								reflected <= "0";
+							when others =>
+								reflected <= "0";
+						end case;
+					when others =>
+						reflected <= "0";
+				end case;
+
+        ar <= ar(31 downto 3) & reflected; -- TODO: make coherent with actual ball-reg structure
+        
       elsif alu_opcode = 15 then 				-- set special flag
         status(7) <= '1';	
       else                              -- idle
@@ -52,6 +70,8 @@ begin
     end if;
   end process;
 
+	normal <= ar(2 downto 0); -- Assumes AR holds normal as 3 least sigbits
+	ball_v <= alu_data(2 downto 0); -- Same assumption
   K_add <= ar + alu_data;
   K_sub <= ar - alu_data;
   equal <= '1' when (ar = alu_data) else '0';
