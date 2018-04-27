cfb69d90f36f88e98c584db9380b853368aa0f8e
William Stenberg <wilst118@ixtab.edu.isy.liu.se>
2018-04-27 14:00:36 +0200

    Vector reflection

--- cpu_embryo/alu.vhd
+++ cpu_embryo/alu.vhd
@@ -21,19 +21,22 @@ architecture Behavioral of alu is
   signal normal : unsigned (2 downto 0); -- Collision normal vector
 	signal ball_v : unsigned (2 downto 0); -- Ball velocity vector
 	signal reflected : unsigned (2 downto 0); -- Reflected velocity vector 
+	signal diff : unsigned (2 downto 0); 	-- Reflection diff (to determine whether to reflect)
   
 begin
   process(clk, normal, ball_v) begin
     if (rising_edge(clk)) then
-      if alu_opcode = 1 then       -- add
+			if alu_opcode = 1 then 					-- Place data in AR
+				ar <= alu_data;
+      elsif alu_opcode = 2 then       -- add
         -- Overflow flag
         if ((ar(31) = alu_data(31)) and (K_add(31) /= ar(31))) then
           status(0) <= '1';
-	else
-	  status(0) <= '0';
-	end if;
+				else
+	 			 status(0) <= '0';
+				end if;
         ar <= ar + alu_data;
-      elsif alu_opcode = 2 then         -- sub
+      elsif alu_opcode = 3 then         -- sub
         -- set overflow flag
         if ((ar(31) = alu_data(31)) and (K_sub(31) /= ar(31))) then
           status(0) <= '1';
@@ -43,24 +46,17 @@ begin
         -- zero flag (when subtracting equal terms)
         status(1) <= equal;
         ar <= ar - alu_data;
-      elsif alu_opcode = 3 then 				-- arithmetic/logic shift left
+      elsif alu_opcode = 4 then 				-- arithmetic/logic shift left
         ar <= ar(30 downto 0) & '0';
-      elsif alu_opcode = 4 then 				-- arithmetic shift right
+      elsif alu_opcode = 5 then 				-- arithmetic shift right
         ar <= ar(31) & ar(31 downto 1);
-      elsif alu_opcode = 5 then         -- vector reflection
-				case normal is
-					when "0" =>
-						case ball_v is
-							when "0" =>
-								reflected <= "0";
-							when others =>
-								reflected <= "0";
-						end case;
-					when others =>
-						reflected <= "0";
-				end case;
+      elsif alu_opcode = 6 then         -- vector reflection
+				if diff > 2 then
+        	ar <= ar(31 downto 3) & ball_v; -- TODO: make coherent with actual ball-reg structure
+				else
+        	ar <= ar(31 downto 3) & reflected; -- TODO: make coherent with actual ball-reg structure
+				end if;
 
-        ar <= ar(31 downto 3) & reflected; -- TODO: make coherent with actual ball-reg structure
         
       elsif alu_opcode = 15 then 				-- set special flag
         status(7) <= '1';	
@@ -72,6 +68,8 @@ begin
 
 	normal <= ar(2 downto 0); -- Assumes AR holds normal as 3 least sigbits
 	ball_v <= alu_data(2 downto 0); -- Same assumption
+	diff <= 5 + normal - ball_v;
+	reflected <= 4 - ball_v + normal + normal;
   K_add <= ar + alu_data;
   K_sub <= ar - alu_data;
   equal <= '1' when (ar = alu_data) else '0';
